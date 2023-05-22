28a915ed058d583e2c994f90622b54bf76aac376
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-05-23 01:03:17 +0300

    Update bot.py
    publish photo to Telegram channel.

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
-import requests
 import os
 from dotenv import load_dotenv
 import telegram
+
 
 def main():
     load_dotenv()
     token = os.environ['TLG_TOKEN']
     chat_id = os.environ['TLG_CHAT_ID']
     bot = telegram.Bot(token)
-    bot.send_message(chat_id=chat_id, text="publish text to Telegram channel.")
-    return  bot.get_me()
-
+    bot.send_document(chat_id=chat_id, document=open('./images/spacex4.jpeg', 'rb'))
+    return bot.get_me()
 
 
 if __name__ == '__main__':

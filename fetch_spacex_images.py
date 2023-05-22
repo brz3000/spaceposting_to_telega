51c92a27b162d2efc42e4b7ec256d0164605dc12
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-05-22 15:45:42 +0300

    Add fetch_spacex_images.py

---
+++ fetch_spacex_images.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from pathlib import Path
+from urllib.parse import urlparse
+from urllib.parse import unquote
+from dotenv import load_dotenv
+from main import download_image
+import argparse
+
+def get_images_list_from_spacex(launch=''):
+    url_template = 'https://api.spacexdata.com/v5/launches/{}'
+    if not launch:
+        launch = 'latest'
+    url = url_template.format(launch)
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['links']['flickr']['original']
+
+if __name__ == '__main__':
+    Path("./images").mkdir(parents=True, exist_ok=True)
+    if not os.path.exists("./images"):
+        os.makedirs("./images")
+    load_dotenv()
+    token = os.environ['NASA_TOKEN']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", default='latest', type=str, help="Введите id запуска")
+    args = parser.parse_args()
+
+    for image_number, image_url in enumerate(get_images_list_from_spacex(args.l)):
+        filename = f'./images/spacex{image_number}.jpeg'
+        download_image(image_url, filename)
+

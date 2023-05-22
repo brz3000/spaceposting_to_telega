c243da5192321861364858e2859ce06df5bfe7b9
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-05-23 00:36:42 +0300

    Add bot.py
    publish text to Telegram channel

--- bot.py
+++ bot.py
@@ -6,8 +6,11 @@ import telegram
 def main():
     load_dotenv()
     token = os.environ['TLG_TOKEN']
+    chat_id = os.environ['TLG_CHAT_ID']
     bot = telegram.Bot(token)
+    bot.send_message(chat_id=chat_id, text="publish text to Telegram channel.")
     return  bot.get_me()
+
 
 
 if __name__ == '__main__':

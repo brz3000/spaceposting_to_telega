40dc33223be34b448ef3377accb1bc6708817154
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-06-10 23:13:02 +0300

    Update fetch_epic_images.py
    Rename viriables

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -1,22 +1,21 @@
 import requests
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 from download_image import download_image
 
 
-def get_epic_image_list(token):
+def get_epic_images(token):
     url = 'https://api.nasa.gov/EPIC/api/natural/images'
     params = {'api_key': token}
     response = requests.get(url=url, params=params)
     response.raise_for_status()
-    epic_list = []
+    epics = []
     for epic in response.json():
         image = epic['image']
         year, month, day = epic['date'].split(' ')[0].split('-')
         epic_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png'
-        epic_list.append(epic_url)
-    return epic_list
+        epics.append(epic_url)
+    return epics
 
 
 if __name__ == '__main__':
@@ -24,7 +23,7 @@ if __name__ == '__main__':
     load_dotenv()
     token = os.environ['NASA_TOKEN']
 
-    for image_number, image_url in enumerate(get_epic_image_list(token)):
+    for image_number, image_url in enumerate(get_epic_images(token)):
         image_url = f'{image_url}?api_key={token}'
         filename = f'./images/nasa_epic_{image_number}.png'
         download_image(image_url, filename)

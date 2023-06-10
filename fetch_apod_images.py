2ead12c49e7f785e70703d803ecc7102cc36ca64
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-06-10 23:12:58 +0300

    Update fetch_apod_images.py
    Rename viriables

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -1,6 +1,5 @@
 import requests
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 from download_image import download_image
 from urllib.parse import urlparse
@@ -12,22 +11,22 @@ def get_extention_from_url(url):
     return f'{extention}'
 
 
-def get_apod_image_list(token, count=30):
+def get_apod_images(token, count=30):
     url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': token, 'count': count}
     response = requests.get(url, params=params)
     response.raise_for_status()
-    apod_url_list = []
+    apod_urls = []
     for apod in response.json():
-        apod_url_list.append(apod['url'])
-    return apod_url_list
+        apod_urls.append(apod['url'])
+    return apod_urls
 
 
 if __name__ == '__main__':
-    os.makedirs("./images", exist_ok=False)
+    os.makedirs("./images", exist_ok=True)
     load_dotenv()
     token = os.environ['NASA_TOKEN']
 
-    for image_number, image_url in enumerate(get_apod_image_list(token)):
+    for image_number, image_url in enumerate(get_apod_images(token)):
         filename = f'./images/nasa_apod_{image_number}{get_extention_from_url(image_url)}'
         download_image(image_url, filename)

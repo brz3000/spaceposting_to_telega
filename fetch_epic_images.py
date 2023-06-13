1c21dfbcef5bbda239fba236aaeccde2654d75e8
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-06-14 00:34:22 +0300

    Update fetch_epic_images.py
    Fix params for url

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 from download_image import download_image
+import urllib.parse
+
 
 
 def get_epic_images(token):
@@ -22,8 +24,9 @@ if __name__ == '__main__':
     os.makedirs("./images", exist_ok=True)
     load_dotenv()
     token = os.environ['NASA_TOKEN']
+    params = {'api_key': token}
 
     for image_number, image_url in enumerate(get_epic_images(token)):
-        image_url = f'{image_url}?api_key={token}'
-        filename = f'./images/nasa_epic_{image_number}.png'
-        download_image(image_url, filename)
+        file_path = f'./images/nasa_epic_{image_number}.png'
+        download_image(image_url, file_path, params)
+
